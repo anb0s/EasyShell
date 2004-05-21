5812a61ec6dc25751571493341b1e26a5f425552
Marcel Schoen <marcelschoen@users.sourceforge.net>
2004-05-21 13:36:41 +0000

    *** empty log message ***

--- easyshell/src/com/tetrade/eclipse/plugins/easyshell/preferences/EasyShellPreferencePage.java
+++ easyshell/src/com/tetrade/eclipse/plugins/easyshell/preferences/EasyShellPreferencePage.java
@@ -13,6 +13,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -27,15 +28,20 @@ public class EasyShellPreferencePage
     implements IWorkbenchPreferencePage {
 
     public static final String P_TARGET = "targetPreference";
+    public static final String P_LIST = "listPreference";
 
     private static String[] defaultCmds = {
     		"cmd.exe /C start cmd.exe /K \"{0}: && cd {1} \"",
-			"konsole --noclose --workdir {1}"};
+			"konsole --noclose --workdir {1}",
+			"xterm"};
     private static String[] cmdLabels = {
-    		"Windows DOS-Shell", "KDE Konsole"
+    		"Windows DOS-Shell", "KDE Konsole", "Xterm"
     };
+    private static int cmdWinDOS = 0;
+    private static int cmdKonsole = 1;
+    private static int cmdXterm = 2;
     
-    private Combo categoryCombo = null;
+    private Combo targetCombo = null;
     private StringFieldEditor editor = null;
 
 	// for unixes    
@@ -48,6 +54,7 @@ public class EasyShellPreferencePage
         setDescription("Set up a shell command window.");
         initializeDefaults();
     }
+
     /**
      * Sets the default values of the preferences.
      */
@@ -59,7 +66,7 @@ public class EasyShellPreferencePage
         String target = null;
         String osname = System.getProperty("os.name", "").toLowerCase();
         if (osname.indexOf("windows") != -1) {
-            target = "cmd.exe /C start cmd.exe /K \"{0}: && cd {1} \"";
+            target = defaultCmds[cmdWinDOS];
         } else if (
             osname.indexOf("unix") != -1
                 || osname.indexOf("irix") != -1
@@ -70,11 +77,11 @@ public class EasyShellPreferencePage
                 || osname.indexOf("linux") != -1) {
             int desktop = detectDesktop();
             if(desktop == DESKTOP_KDE) {
-				target = "konsole --noclose --workdir {1}";
+				target = defaultCmds[cmdKonsole];
             } else if(desktop == DESKTOP_GNOME) {
-				target = "konsole --noclose --workdir {1}";
+				target = defaultCmds[cmdKonsole];
 			} else if(desktop == DESKTOP_CDE) {
-				target = "xterm";
+				target = defaultCmds[cmdXterm];
             }
         }
         store.setDefault(P_TARGET, target);
@@ -88,62 +95,70 @@ public class EasyShellPreferencePage
      * restore itself.
      */
 	protected Control createContents(Composite parent) {
+
+		IPreferenceStore store = getPreferenceStore();
+
 		Composite mainColumn = new Composite(parent, SWT.NONE);
+		GridLayout mainLayout = new GridLayout(1, false);
+		mainColumn.setFont(parent.getFont());
+		mainColumn.setLayout(mainLayout);
+		
+		Composite targetColumn = new Composite(mainColumn, SWT.NONE);
 		GridLayout layout = new GridLayout(2, false);
 		layout.marginWidth = 0;
 		layout.marginHeight = 0;
-		mainColumn.setFont(parent.getFont());
-		mainColumn.setLayout(layout);
-		
-        categoryCombo = new Combo(mainColumn, SWT.READ_ONLY);
+		targetColumn.setFont(parent.getFont());
+		targetColumn.setLayout(layout);
+
+        Label label = new Label(targetColumn, 0);
+        label.setText("Presets");
+
+        targetCombo = new Combo(targetColumn, SWT.READ_ONLY);
         for(int i = 0; i < cmdLabels.length; i++) {
-        	categoryCombo.add(cmdLabels[i]);
+        	targetCombo.add(cmdLabels[i]);
         }
+        targetCombo.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                refreshTarget();
+            }
+        });        
+        targetCombo.select(store.getInt(P_LIST));
         
         editor = new StringFieldEditor(
                 P_TARGET,
-                "&Target:",
-                mainColumn);
+                "&Shell command:",
+                targetColumn);
+        System.out.println("Value: " + store.getString(P_TARGET));
+        System.out.println("Default: " + store.getDefaultString(P_TARGET));
+        editor.setStringValue(store.getString(P_TARGET));
         
-        hookListeners();
+        label = new Label(mainColumn, 0);
+        label.setText("Argument {0} is the drive letter on Win32.");
+        label = new Label(mainColumn, 0);
+        label.setText("Argument {1} is the path.");
         
         return mainColumn;
-	}
-	
-	private void hookListeners() {
-        categoryCombo.addSelectionListener(new SelectionAdapter() {
-            public void widgetSelected(SelectionEvent e) {
-                refreshCategory();
-            }
-        });        
 	}
 	
     /**
      * Refreshes teh category.
      */
-    private void refreshCategory() {
-        int index = categoryCombo.getSelectionIndex();
+    private void refreshTarget() {
+        int index = targetCombo.getSelectionIndex();
         String text = defaultCmds[index];
         System.out.println("Set text to " + text);
         editor.setStringValue(text);
+        IPreferenceStore store = getPreferenceStore();
+        store.setValue(P_TARGET, text);
+    }
+    
+    public boolean performOk() {
+        IPreferenceStore store = getPreferenceStore();
+        store.setValue(P_TARGET, editor.getStringValue());
+        store.setValue(P_LIST, targetCombo.getSelectionIndex());
+    	return true;
     }
 
-    /**
-     * Creates the field editors. Field editors are abstractions of
-     * the common GUI blocks needed to manipulate various types
-     * of preferences. Each field editor knows how to save and
-     * restore itself.
-     */
-/*
-    public void createFieldEditors() {
-
-        addField(
-            new StringFieldEditor(
-                P_TARGET,
-                "&Target:",
-                getFieldEditorParent()));
-    }
-*/
     public void init(IWorkbench workbench) {
     }
 
