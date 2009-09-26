e4967c6d83d61cc2c7bd3299648474fc15fe6087
Andre Bossert <anb0s@users.sourceforge.net>
2009-09-26 08:13:52 +0000

    + PowerShell support

--- easyshell/src/com/tetrade/eclipse/plugins/easyshell/preferences/EasyShellPreferencePage.java
+++ easyshell/src/com/tetrade/eclipse/plugins/easyshell/preferences/EasyShellPreferencePage.java
@@ -36,18 +36,20 @@ public class EasyShellPreferencePage
 
     private static String[] defaultCmdsOpen = {
     	"cmd.exe /C start \"{4}\" /D\"{1}\" cmd.exe /K",
-    	"cmd.exe /C start \"{4}\" /D\"{1}\" cmd.exe /K \"bash.exe\"",
-    	//"cmd.exe /C start \"{4}\" /D\"{1}\" cmd.exe /K \"set HOME={1} && bash.exe --login -i\"",
+    	"cmd.exe /C start \"{4}\" /D\"{1}\" powershell.exe",
+    	"cmd.exe /C start \"{4}\" /D\"{1}\" bash.exe",
 		"konsole --noclose --workdir \"{1}\"",
 		"gnome-terminal --working-directory=\"{1}\"",
 		"cd \"{1}\" && xterm"};
     private static String[] defaultCmdsRun = {
 		"cmd.exe /C start \"{4}\" /D\"{1}\" \"{3}\"",
-		"cmd.exe /C start \"{4}\" /D\"{1}\" cmd.exe /K \"bash.exe -c ./{3}\"",
+		"cmd.exe /C start \"{4}\" /D\"{1}\" powershell.exe -command \"./{3}\"",
+		"cmd.exe /C start \"{4}\" /D\"{1}\" bash.exe -c \"./{3}\"",
 		"konsole --noclose --workdir \"{1}\" -e \"./{3}\"",
 		"gnome-terminal --working-directory=\"{1}\" --command=\"./{3}\"",
 		"cd \"{1}\" && xterm -e \"./{3}\""};
     private static String[] defaultCmdsExplore = {
+		"explorer.exe /select,\"{2}\"",
 		"explorer.exe /select,\"{2}\"",
 		"explorer.exe /select,\"{2}\"",
 		"konqueror file:\"{1}\"",
@@ -55,7 +57,7 @@ public class EasyShellPreferencePage
 		//"gnome-open \"{1}\""
     	"cd \"{1}\" && dtfile"};
     private static String[] cmdLabels = {
-    		"Windows DOS-Shell / Explorer", "Windows Cygwin (Bash) / Explorer", "KDE Konsole / Konqueror", "Gnome Terminal / Nautilus", "CDE Xterm / Dtfile"
+    		"Windows DOS-Shell / Explorer", "Windows PowerShell / Explorer", "Windows Cygwin (Bash) / Explorer", "KDE Konsole / Konqueror", "Gnome Terminal / Nautilus", "CDE Xterm / Dtfile"
     };
     private static int cmdWinDOS = 0;
     private static int cmdWinCyg = 1;
