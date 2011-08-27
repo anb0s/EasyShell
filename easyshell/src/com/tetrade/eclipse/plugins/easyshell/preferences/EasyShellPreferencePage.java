03eab99ac70e6d40ca962ae9b49a921f2d14948c
Andre Bossert <anb0s@users.sourceforge.net>
2011-08-27 19:47:23 +0000

    * fixed issues:  * ID: 3197997: Exception in Mac OS  * ID: 3157616: Preference Page Error
    * reworked the logic of presets
    * added new "Unknown" preset for all systems
    * added new preset for "Mac OS X, Terminal / Finder"

--- easyshell/src/com/tetrade/eclipse/plugins/easyshell/preferences/EasyShellPreferencePage.java
+++ easyshell/src/com/tetrade/eclipse/plugins/easyshell/preferences/EasyShellPreferencePage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 - 2010 by Marcel Schoen and Andre Bossert
+ * Copyright (C) 2004 - 2011 by Marcel Schoen and Andre Bossert
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -52,54 +52,64 @@ public class EasyShellPreferencePage
     public static final String P_TARGET_EXPLORE = "targetExplorePreference";
     public static final String P_TARGET_COPYPATH = "targetCopyPathPreference";
     public static final String P_LIST = "listPreference";
+    public static final String P_LIST_STR = "IdStr";
 
-    public static final int numConfigs = 6;
-    
-    private static String[] cmdLabels = {
-		"Windows DOS-Shell / Explorer",
-		"Windows PowerShell / Explorer",
-		"Windows Cygwin (Bash) / Explorer",
-		"KDE Konsole / Konqueror",
-		"Gnome Terminal / Nautilus",
-		"CDE Xterm / Dtfile"
-    };
-    private static String[] defaultCmdsOpen = {
-    	"cmd.exe /C start \"{4}\" /D\"{1}\" cmd.exe /K",
-    	"cmd.exe /C start \"{4}\" /D\"{1}\" powershell.exe",
-    	"cmd.exe /C start \"{4}\" /D\"{1}\" bash.exe",
-		"konsole --noclose --workdir {1}",
-		"gnome-terminal --working-directory={1}",
-		"cd {1} && xterm"
-		};
-    private static String[] defaultCmdsRun = {
-		"cmd.exe /C start \"{4}\" /D\"{1}\" \"{3}\"",
-		"cmd.exe /C start \"{4}\" /D\"{1}\" powershell.exe -command \"./{3}\"",
-		"cmd.exe /C start \"{4}\" /D\"{1}\" bash.exe -c \"./{3}\"",
-		"konsole --noclose --workdir {1} -e ./{3}",
-		"gnome-terminal --working-directory={1} --command=./{3}",
-		"cd {1} && xterm -e ./{3}"
-		};
-    private static String[] defaultCmdsExplore = {
-		"explorer.exe /select,\"{2}\"",
-		"explorer.exe /select,\"{2}\"",
-		"explorer.exe /select,\"{2}\"",
-		"konqueror file:{2}",
-		"nautilus {2}",
-    	"cd {1} && dtfile"
-		};
-    private static String[] defaultCmdsCopyPath = {
-		"{2}{5}",
-		"{2}{5}",
-		"{2}{5}",
-		"{2}{5}",
-		"{2}{5}",
-		"{2}{5}",
-    };
-    private static int cmdWinDOS = 0;
-    private static int cmdWinCyg = 1;
-    private static int cmdKonsoleKDE = 2;
-    private static int cmdKonsoleGnome = 3;
-    private static int cmdXterm = 4;
+    //public static final int numConfigs = 6;
+
+    // Commands
+    private enum EasyShellCommand {
+        cmdUnknown(0, "Unknown shell / file browser", 		"open {1}", 											"cd {1} && run {3}",													"explore {2}",					"{2}{5}"),
+        cmdWinDOS(1, "Windows DOS-Shell / Explorer", 		"cmd.exe /C start \"{4}\" /D\"{1}\" cmd.exe /K", 		"cmd.exe /C start \"{4}\" /D\"{1}\" \"{3}\"", 							"explorer.exe /select,\"{2}\"",	"{2}{5}"),
+        cmdWinPower(2, "Windows PowerShell / Explorer", 	"cmd.exe /C start \"{4}\" /D\"{1}\" powershell.exe",	"cmd.exe /C start \"{4}\" /D\"{1}\" powershell.exe -command \"./{3}\"", "explorer.exe /select,\"{2}\"", "{2}{5}"),
+        cmdWinCyg(3, "Windows Cygwin (Bash) / Explorer", 	"cmd.exe /C start \"{4}\" /D\"{1}\" bash.exe", 			"cmd.exe /C start \"{4}\" /D\"{1}\" bash.exe -c \"./{3}\"", 			"explorer.exe /select,\"{2}\"", "{2}{5}"),
+        cmdKonsoleKDE(4, "KDE Konsole / Konqueror", 		"konsole --noclose --workdir {1}", 						"konsole --noclose --workdir {1} -e ./{3}", 							"konqueror file:{2}", 			"{2}{5}"),
+        cmdKonsoleGnome(5, "Gnome Terminal / Nautilus", 	"gnome-terminal --working-directory={1}", 				"gnome-terminal --working-directory={1} --command=./{3}", 				"nautilus {2}", 				"{2}{5}"),
+        cmdXtermDtfile(6, "CDE Xterm / Dtfile", 			"cd {1} && xterm", 										"cd {1} && xterm -e ./{3}", 											"cd {1} && dtfile", 			"{2}{5}"),
+        cmdTerminalFinder(7, "MAC OS X Terminal / Finder",	"open -a Terminal {1}", 								"cd {1} && open {3}",													"open -a Finder {2}",			"{2}{5}");
+        // attributes
+        private final int id;
+        private final String label;
+        private final String openCmd;
+        private final String runCmd;
+        private final String exploreCmd;
+        private final String copyPathCmd;
+        // construct
+        EasyShellCommand(int id, String label, String openCmd, String runCmd, String exploreCmd, String copyPathCmd) {
+        	this.id = id;
+        	this.label = label;
+        	this.openCmd = openCmd;
+        	this.runCmd = runCmd;
+        	this.exploreCmd = exploreCmd;
+        	this.copyPathCmd = copyPathCmd;
+        }
+		public int getId() {
+			return id;
+		}
+		public String getLabel() {
+			return label;
+		}
+		public String getOpenCmd() {
+			return openCmd;
+		}
+		public String getRunCmd() {
+			return runCmd;
+		}
+		public String getExploreCmd() {
+			return exploreCmd;
+		}
+		public String getCopyPathCmd() {
+			return copyPathCmd;
+		}
+		static EasyShellCommand getCommandFromId(int id) {
+			EasyShellCommand ret = cmdUnknown;
+	        for(int i = 0; i < EasyShellCommand.values().length; i++) {
+	        	if (EasyShellCommand.values()[i].getId() == id) {
+	        		ret = EasyShellCommand.values()[i];
+	        	}
+	        }
+	        return ret;
+		}
+    }
 
     private Combo targetCombo = null;
     private StringFieldEditor targetOpenEditor = null;
@@ -125,31 +135,60 @@ public class EasyShellPreferencePage
     private void initializeDefaults() {
         IPreferenceStore store = getPreferenceStore();
 
-        int cmdNum = -1;
+        EasyShellCommand cmd = EasyShellCommand.cmdUnknown;
+        /* possible OS string:
+            AIX
+			Digital UNIX
+			FreeBSD
+			HP UX
+			Irix
+			Linux
+			Mac OS
+			Mac OS X
+			MPE/iX
+			Netware 4.11
+			OS/2
+			Solaris
+			Windows 95
+			Windows 98
+			Windows NT
+			Windows Me
+			Windows 2000
+			Windows XP
+			Windows 2003
+			Windows CE
+			Windows Vista
+			Windows 7
+         */
         String osname = System.getProperty("os.name", "").toLowerCase();
         if (osname.indexOf("windows") != -1) {
-        	cmdNum = cmdWinDOS;
+        	cmd = EasyShellCommand.cmdWinDOS;
         } else if (
-            osname.indexOf("unix") != -1
+        		   osname.indexOf("unix") != -1
                 || osname.indexOf("irix") != -1
                 || osname.indexOf("freebsd") != -1
                 || osname.indexOf("hp-ux") != -1
                 || osname.indexOf("aix") != -1
                 || osname.indexOf("sunos") != -1
-                || osname.indexOf("linux") != -1) {
+                || osname.indexOf("linux") != -1
+        		)
+        {
             int desktop = detectDesktop();
             if(desktop == DESKTOP_KDE) {
-            	cmdNum = cmdKonsoleKDE;
+            	cmd = EasyShellCommand.cmdKonsoleKDE;
             } else if(desktop == DESKTOP_GNOME) {
-            	cmdNum = cmdKonsoleGnome;
+            	cmd = EasyShellCommand.cmdKonsoleGnome;
 			} else if(desktop == DESKTOP_CDE) {
-				cmdNum = cmdXterm;
+				cmd = EasyShellCommand.cmdXtermDtfile;
             }
+        } else if (osname.indexOf("mac os x") != -1) {
+        	cmd = EasyShellCommand.cmdTerminalFinder;
         }
-        store.setDefault(P_TARGET, defaultCmdsOpen[cmdNum]);
-        store.setDefault(P_TARGET_RUN, defaultCmdsRun[cmdNum]);
-        store.setDefault(P_TARGET_EXPLORE, defaultCmdsExplore[cmdNum]);
-        store.setDefault(P_TARGET_COPYPATH, defaultCmdsCopyPath[cmdNum]);
+        // set default commands
+        store.setDefault(P_TARGET, cmd.getOpenCmd());
+        store.setDefault(P_TARGET_RUN, cmd.getRunCmd());
+        store.setDefault(P_TARGET_EXPLORE, cmd.getExploreCmd());
+        store.setDefault(P_TARGET_COPYPATH, cmd.getCopyPathCmd());
     }
 
     /**
@@ -178,15 +217,20 @@ public class EasyShellPreferencePage
         label.setText("Presets");
 
         targetCombo = new Combo(targetColumn, SWT.READ_ONLY | SWT.DROP_DOWN);
-        for(int i = 0; i < cmdLabels.length; i++) {
-        	targetCombo.add(cmdLabels[i]);
+        for(int i = 0; i < EasyShellCommand.values().length; i++) {
+        	targetCombo.add(EasyShellCommand.getCommandFromId(i).getLabel());
         }
         targetCombo.addSelectionListener(new SelectionAdapter() {
             public void widgetSelected(SelectionEvent e) {
                 refreshTarget();
             }
         });
-        targetCombo.select(store.getInt(P_LIST));
+        targetCombo.select(store.getInt(P_LIST) + 1);
+        // get new string value
+        String IdStr = store.getString(P_LIST_STR);
+        if (IdStr != null && IdStr.length() > 0) {
+        	targetCombo.select(EasyShellCommand.valueOf(IdStr).getId());
+        }
 
         targetOpenEditor = new StringFieldEditor(
                 P_TARGET,
@@ -219,7 +263,7 @@ public class EasyShellPreferencePage
         if (debug) System.out.println("Value: " + store.getString(P_TARGET_COPYPATH));
         if (debug) System.out.println("Default: " + store.getDefaultString(P_TARGET_COPYPATH));
         targetCopyPathEditor.setStringValue(store.getString(P_TARGET_COPYPATH));
-        
+
         label = new Label(mainColumn, 0);
         label.setText("Argument {0} is the drive letter on Win32.");
         label = new Label(mainColumn, 0);
@@ -241,10 +285,11 @@ public class EasyShellPreferencePage
      */
     private void refreshTarget() {
         int index = targetCombo.getSelectionIndex();
-        String textOpen = defaultCmdsOpen[index];
-        String textRun = defaultCmdsRun[index];
-        String textExplore = defaultCmdsExplore[index];
-        String textCopyPath = defaultCmdsCopyPath[index];
+        EasyShellCommand cmd = EasyShellCommand.getCommandFromId(index);
+        String textOpen = cmd.getOpenCmd();
+        String textRun = cmd.getRunCmd();
+        String textExplore = cmd.getExploreCmd();
+        String textCopyPath = cmd.getCopyPathCmd();
         if (debug) System.out.println("Set open text to " + textOpen);
         targetOpenEditor.setStringValue(textOpen);
         if (debug) System.out.println("Set run text to " + textRun);
@@ -266,7 +311,8 @@ public class EasyShellPreferencePage
         store.setValue(P_TARGET_RUN, targetRunEditor.getStringValue());
         store.setValue(P_TARGET_EXPLORE, targetExploreEditor.getStringValue());
         store.setValue(P_TARGET_COPYPATH, targetCopyPathEditor.getStringValue());
-        store.setValue(P_LIST, targetCombo.getSelectionIndex());
+        store.setValue(P_LIST, targetCombo.getSelectionIndex() - 1);
+        store.setValue(P_LIST_STR, EasyShellCommand.getCommandFromId(targetCombo.getSelectionIndex()).name());
     	return true;
     }
 
@@ -287,14 +333,15 @@ public class EasyShellPreferencePage
 		int resultCode = -1;
 		try {
 			String[] cmd = new String[1];
-			cmd[0] = "env";
+			cmd[0] = "env"; // or echo $DESKTOP_SESSION
 			Process proc = Runtime.getRuntime().exec(cmd);
-
 			BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 			String line = null;
 			while((line = in.readLine()) != null && resultCode == -1) {
 				if(line.indexOf("KDE") != -1) {
 					resultCode = DESKTOP_KDE;
+				} else if(line.toLowerCase().indexOf("gnome") != -1) {
+					resultCode = DESKTOP_GNOME;
 				}
 			}
 
