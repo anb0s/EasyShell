1b480aa25066841c11e58486d7e19c2740cbb7ec
Andre Bossert <anb0s@anbos.de>
2015-03-22 11:34:50 +0100

    created new EasyShell plugin, feature and update site from scratch

---
+++ de.anbos.eclipse.easyshell.plugin/src/de/anbos/eclipse/easyshell/plugin/preferences/PreferenceConstants.java
@@ -0,0 +1,16 @@
+package de.anbos.eclipse.easyshell.plugin.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_PATH = "pathPreference";
+
+	public static final String P_BOOLEAN = "booleanPreference";
+
+	public static final String P_CHOICE = "choicePreference";
+
+	public static final String P_STRING = "stringPreference";
+	
+}
