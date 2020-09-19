f389f87c2f1c328755910b168635f9f792f6c4ae
Andre Bossert <anb0s@anbos.de>
2020-09-19 18:19:46 +0200

    Update EPL 2.0 and copyright year 2020 to all files
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- plugin/src/de/anbos/eclipse/easyshell/plugin/preferences/TypedComboBoxSelectionListener.java
+++ plugin/src/de/anbos/eclipse/easyshell/plugin/preferences/TypedComboBoxSelectionListener.java
@@ -1,17 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2014 - 2018 Andre Bossert.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * Copyright (c) 2014-2020 Andre Bossert <anb0s@anbos.de>.
  *
- * Contributors:
- *    Andre Bossert - initial API and implementation and/or initial documentation
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
- *    adapted code from:
- *    http://stackoverflow.com/questions/12511959/is-there-swt-combo-box-with-any-object-as-data-and-labelprovider-for-display
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
  *
- *******************************************************************************/
+ * SPDX-License-Identifier: EPL-2.0
+ */
 
 package de.anbos.eclipse.easyshell.plugin.preferences;
 
