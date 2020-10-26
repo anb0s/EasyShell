16e7b66450ea5f8ba9bdcffd056a8d77be7f8756
Andre Bossert <anb0s@anbos.de>
2020-10-26 22:23:02 +0100

    remove comments
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- plugin/src/de/anbos/eclipse/easyshell/plugin/actions/Action.java
+++ plugin/src/de/anbos/eclipse/easyshell/plugin/actions/Action.java
@@ -30,43 +30,35 @@ public class Action implements IAction {
 
     @Override
     public void addPropertyChangeListener(IPropertyChangeListener listener) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public int getAccelerator() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public String getActionDefinitionId() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public ImageDescriptor getDisabledImageDescriptor() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public HelpListener getHelpListener() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public ImageDescriptor getHoverImageDescriptor() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -77,110 +69,82 @@ public class Action implements IAction {
 
     @Override
     public ImageDescriptor getImageDescriptor() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public IMenuCreator getMenuCreator() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public int getStyle() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public String getText() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getToolTipText() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean isChecked() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isHandled() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public void removePropertyChangeListener(IPropertyChangeListener listener) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void runWithEvent(Event event) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setActionDefinitionId(String id) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setChecked(boolean checked) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setDescription(String text) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setDisabledImageDescriptor(ImageDescriptor newImage) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setEnabled(boolean enabled) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setHelpListener(HelpListener listener) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setHoverImageDescriptor(ImageDescriptor newImage) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -190,31 +154,21 @@ public class Action implements IAction {
 
     @Override
     public void setImageDescriptor(ImageDescriptor newImage) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setMenuCreator(IMenuCreator creator) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setText(String text) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setToolTipText(String text) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setAccelerator(int keycode) {
-        // TODO Auto-generated method stub
-
     }
 }
